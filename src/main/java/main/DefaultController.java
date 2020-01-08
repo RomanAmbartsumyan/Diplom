f791720bbd7a1a1b55ae8292a90cff94021a7aa5
Роман Амбарцумян <r9854334307@mail.ru>
2020-01-08 18:25:24 +0300

    Maven, controller added, frontend connected

---
+++ src/main/java/main/DefaultController.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
