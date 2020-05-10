6138736dc121902e02afabced0e8baf1bd9c4507
Roman Ambartsumyan <r9854334307@mail.ru>
2020-05-10 22:38:56 +0300

    correction

--- src/main/java/project/controllers/rest/ApiTagController.java
+++ src/main/java/project/controllers/rest/ApiTagController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.dto.TagDto;
 import project.dto.TagsDto;
@@ -14,13 +13,12 @@ import project.services.PostService;
 import project.services.TagService;
 import project.services.TagToPostService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Контроллер тэгов
@@ -32,12 +30,12 @@ public class ApiTagController {
     private final TagService tagService;
     private final PostService postService;
     private final TagToPostService tagToPostService;
+
     /**
      * Выдает тэги
      */
     @GetMapping
-    public ResponseEntity<TagsDto> getTagByName(@RequestParam(required = false) String query){
-        List<TagDto> tagsDto = new ArrayList<>();
+    public ResponseEntity<TagsDto> getTagByName() {
         Integer countPostsActiveAndModerationAccept =
                 postService.countPostsByActiveAndModerationStatus((byte) 1, ModerationStatus.ACCEPTED);
 
@@ -45,20 +43,13 @@ public class ApiTagController {
 
         List<Tag> tags = tagIds.stream().map(tagService::getTagById).collect(toList());
 
-        Map<String, Integer> tagsAngPosts = new HashMap<>();
+        Map<String, Integer> tagsAngPosts = tags.stream()
+                .collect(toMap(Tag::getName, tag -> tagToPostService.countPostsWithTag(tag.getId())));
 
-        tags.forEach(tag -> {
-            Integer countPosts = tagToPostService.countPostsWithTag(tag.getId());
-            tagsAngPosts.put(tag.getName(), countPosts);
-        });
-
-        tagsAngPosts.forEach((k,v) -> {
-            Float weight = (float) v / countPostsActiveAndModerationAccept;
-            TagDto tagDto = new TagDto(k,weight);
-            tagsDto.add(tagDto);
-        });
-
-        tagsDto.sort(comparing(TagDto::getWeight).reversed());
+        List<TagDto> tagsDto = tagsAngPosts.entrySet().stream().map(pair -> {
+            Float weight = (float) pair.getValue() / countPostsActiveAndModerationAccept;
+            return new TagDto(pair.getKey(), weight);
+        }).sorted(comparing(TagDto::getWeight).reversed()).collect(toList());
 
         return ResponseEntity.ok(new TagsDto(tagsDto));
     }
